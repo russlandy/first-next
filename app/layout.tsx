aff1645e45630ce99d517e58d2ead494f3d07be4
russlandy <ruslandabysov@gmail.com>
2024-05-08 23:29:12 +0300

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@
 // }
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
 export default function RootLayout({
   children,
@@ -23,3 +24,12 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
